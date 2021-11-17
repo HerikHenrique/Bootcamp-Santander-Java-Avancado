c4e1f838c52b17742ae3dd4c58de0d6021e70810
HerikHenrique <74871202+HerikHenrique@users.noreply.github.com>
2021-11-17 19:39:35 -0300

    métodos da Interface List

--- src/CollectionAndStreams/Aula1InterfaceList.java
+++ src/CollectionAndStreams/Aula1InterfaceList.java
@@ -19,8 +19,37 @@ public class Aula1InterfaceList {
 
         System.out.println(nomes);
 
+        nomes.set(2, "Larissa");
+
+        System.out.println(nomes);
+        //Sort(); sorteia os elementos da Lista
         Collections.sort(nomes);
 
         System.out.println(nomes);
+        //remove() recebe 2 implementações. 1 - Remove pelo Indice
+        nomes.remove(4);
+
+        System.out.println(nomes);
+        // Remove pelo conteúdo da String
+        nomes.remove("Carlos");
+
+        System.out.println(nomes);
+        // exibir o indice especificado, necessário colocar em uma variável
+        String nome = nomes.get(3);
+
+        System.out.println(nome);
+
+        int tamanho = nomes.size();
+        System.out.println(tamanho);
+
+        boolean temNome = nomes.contains("anderson");
+        System.out.println(temNome);
+
+        boolean meuEmpty = nomes.isEmpty();
+        System.out.println(meuEmpty);
+
+        //indexOf(); se não tiver na lista o resultado é -1
+        int posicao = nomes.indexOf("Larissa");
+        System.out.println(posicao);
     }
 }
